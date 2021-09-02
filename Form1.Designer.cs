3825af07293661adea1c73e8f20100f304a22cd0
root <vixier61@gmail.com>
2021-09-02 23:56:44 +0200

    Dodaj pliki projektów.

---
+++ Form1.Designer.cs
@@ -0,0 +1,290 @@
+﻿
+namespace FishingBot_Csharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.klawisz = new System.Windows.Forms.TextBox();
+            this.delay = new System.Windows.Forms.NumericUpDown();
+            this.waitTime = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pointAndClick = new System.Windows.Forms.CheckBox();
+            this.castOnStart = new System.Windows.Forms.CheckBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.delay)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.waitTime)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(10, 395);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(202, 50);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Top-Left:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Bottom-Right:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Colour:";
+            // 
+            // klawisz
+            // 
+            this.klawisz.Location = new System.Drawing.Point(15, 111);
+            this.klawisz.Name = "klawisz";
+            this.klawisz.ReadOnly = true;
+            this.klawisz.Size = new System.Drawing.Size(68, 20);
+            this.klawisz.TabIndex = 5;
+            this.klawisz.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.klawisz.DoubleClick += new System.EventHandler(this.klawisz_DoubleClick);
+            // 
+            // delay
+            // 
+            this.delay.Location = new System.Drawing.Point(15, 149);
+            this.delay.Maximum = new decimal(new int[] {
+            100000,
+            0,
+            0,
+            0});
+            this.delay.Name = "delay";
+            this.delay.Size = new System.Drawing.Size(68, 20);
+            this.delay.TabIndex = 6;
+            // 
+            // waitTime
+            // 
+            this.waitTime.Location = new System.Drawing.Point(15, 186);
+            this.waitTime.Maximum = new decimal(new int[] {
+            100000,
+            0,
+            0,
+            0});
+            this.waitTime.Name = "waitTime";
+            this.waitTime.Size = new System.Drawing.Size(68, 20);
+            this.waitTime.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(89, 151);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Delay";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(89, 188);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(83, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Wait until recast";
+            // 
+            // pointAndClick
+            // 
+            this.pointAndClick.AutoSize = true;
+            this.pointAndClick.Location = new System.Drawing.Point(15, 212);
+            this.pointAndClick.Name = "pointAndClick";
+            this.pointAndClick.Size = new System.Drawing.Size(97, 17);
+            this.pointAndClick.TabIndex = 10;
+            this.pointAndClick.Text = "Point and Click";
+            this.pointAndClick.UseVisualStyleBackColor = true;
+            // 
+            // castOnStart
+            // 
+            this.castOnStart.AutoSize = true;
+            this.castOnStart.Location = new System.Drawing.Point(15, 235);
+            this.castOnStart.Name = "castOnStart";
+            this.castOnStart.Size = new System.Drawing.Size(85, 17);
+            this.castOnStart.TabIndex = 11;
+            this.castOnStart.Text = "Cast on start";
+            this.castOnStart.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(89, 114);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Press key";
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(58, 63);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(37, 34);
+            this.button2.TabIndex = 15;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(13, 289);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(71, 17);
+            this.radioButton1.TabIndex = 16;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Find color";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(13, 313);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(146, 17);
+            this.radioButton2.TabIndex = 17;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Find and wait for disapear";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(13, 337);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(95, 17);
+            this.radioButton3.TabIndex = 18;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Any movement";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(15, 360);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(68, 20);
+            this.textBox1.TabIndex = 19;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(89, 363);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(132, 13);
+            this.label7.TabIndex = 20;
+            this.label7.Text = "Key to start any movement";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(223, 456);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.castOnStart);
+            this.Controls.Add(this.pointAndClick);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.waitTime);
+            this.Controls.Add(this.delay);
+            this.Controls.Add(this.klawisz);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.delay)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.waitTime)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox klawisz;
+        private System.Windows.Forms.NumericUpDown delay;
+        private System.Windows.Forms.NumericUpDown waitTime;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.CheckBox pointAndClick;
+        private System.Windows.Forms.CheckBox castOnStart;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,346 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace FishingBot_Csharp
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint cButtons, uint dwExtraInfo);
+        //Mouse actions
+        private const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        private const int MOUSEEVENTF_LEFTUP = 0x04;
+        private const int MOUSEEVENTF_RIGHTDOWN = 0x08;
+        private const int MOUSEEVENTF_RIGHTUP = 0x10;
+
+        public Point punkt1;
+        public Point punkt2;
+        public Point punkt3;
+        public Color bobberColor1;
+        public Boolean anyMovement = false;
+
+        Bitmap bitmap;
+        Bitmap terenDoSzukania;
+        Bitmap terenDoPorownania;
+        Graphics g;
+
+        TypeConverter converter = TypeDescriptor.GetConverter(typeof(Keys));
+
+        private KeyHandler ghk;
+
+        public Form1()
+        {
+            InitializeComponent();
+            ghk = new KeyHandler(Keys.Escape, this);
+            ghk.Register();
+            ghk = new KeyHandler(Keys.K, this);
+            ghk.Register();
+            ghk = new KeyHandler(Keys.J, this);
+            ghk.Register();
+            ghk = new KeyHandler(Keys.H, this);
+            ghk.Register();
+        }
+
+        private void HandleHotkey(ref Message m)
+        {
+            try
+            {
+                this.Cursor = new Cursor(Cursor.Current.Handle);
+                switch ((int)m.LParam)
+                {
+                    case 4718592:
+                        punkt1 = new Point(Cursor.Position.X, Cursor.Position.Y);
+                        label1.Text = "Top-Left: " + Cursor.Position.X.ToString() + "x" + Cursor.Position.Y.ToString();
+                        break;
+                    case 4849664:
+                        punkt2 = new Point(Cursor.Position.X, Cursor.Position.Y);
+                        label2.Text = "Bottom-Right: " + Cursor.Position.X.ToString() + "x" + Cursor.Position.Y.ToString();
+                        break;
+                    case 4915200:
+                        punkt3 = new Point(Cursor.Position.X, Cursor.Position.Y);
+                        Cursor.Position = new Point(0, 0);
+                        System.Threading.Thread.Sleep(100);
+                        terenDoSzukania = new Bitmap(punkt2.X - punkt1.X, punkt2.Y - punkt1.Y);
+                        terenDoPorownania = new Bitmap(punkt2.X - punkt1.X, punkt2.Y - punkt1.Y);
+                        g = Graphics.FromImage(terenDoSzukania);
+                        g.CopyFromScreen(punkt1.X, punkt1.Y, 0, 0, terenDoSzukania.Size);
+                        if ((punkt3.X > punkt1.X && punkt3.X < punkt2.X) && (punkt3.Y > punkt1.Y && punkt3.Y < punkt2.Y))
+                        {
+                            bobberColor1 = terenDoSzukania.GetPixel(punkt3.X - punkt1.X, punkt3.Y - punkt1.Y);
+                            button2.BackColor = bobberColor1;
+                        }
+                        Cursor.Position = punkt3;
+                        g.Dispose();
+                        break;
+                    case 1769472:
+                        this.Close();
+                        break;
+                    default:
+                        anyMovement = !anyMovement;
+                        break;
+                }
+            }
+            catch (System.NullReferenceException)
+            {
+
+            }
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == 0x0312)
+                HandleHotkey(ref m);
+            base.WndProc(ref m);
+        }
+
+        Point ColorFinder()
+        {
+            g = Graphics.FromImage(terenDoSzukania);
+            g.CopyFromScreen(punkt1.X, punkt1.Y, 0, 0, terenDoSzukania.Size);
+            for(int y = 0; y < terenDoSzukania.Height; y++)
+            {
+                for(int x = 0; x < terenDoSzukania.Width; x++)
+                {
+                    if(terenDoSzukania.GetPixel(x, y).R > bobberColor1.R - 15 &&
+                        terenDoSzukania.GetPixel(x, y).G > bobberColor1.G - 15 &&
+                        terenDoSzukania.GetPixel(x, y).B > bobberColor1.B - 15 &&
+                        terenDoSzukania.GetPixel(x, y).R < bobberColor1.R + 15 &&
+                        terenDoSzukania.GetPixel(x, y).G < bobberColor1.G + 15 &&
+                        terenDoSzukania.GetPixel(x, y).B < bobberColor1.B + 15)
+                    {
+                        return new Point(x+punkt1.X, y+punkt1.Y);
+                    }
+                }
+            }
+            return new Point(0, 0);
+        }
+
+        bool ColorFinder(Point bobberPlace)
+        {
+            g = Graphics.FromImage(bitmap);
+            g.CopyFromScreen(bobberPlace.X, bobberPlace.Y, 0, 0, bitmap.Size);
+
+            for (int y = 0; y < bitmap.Height; y++)
+            {
+                for (int x = 0; x < bitmap.Width; x++)
+                {
+                    if (bitmap.GetPixel(x, y).R > bobberColor1.R - 15 &&
+                        bitmap.GetPixel(x, y).G > bobberColor1.G - 15 &&
+                        bitmap.GetPixel(x, y).B > bobberColor1.B - 15 &&
+                        bitmap.GetPixel(x, y).R < bobberColor1.R + 15 &&
+                        bitmap.GetPixel(x, y).G < bobberColor1.G + 15 &&
+                        bitmap.GetPixel(x, y).B < bobberColor1.B + 15)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public static bool CompareBitmaps(Bitmap bmp1, Bitmap bmp2)
+        {
+            for (int y=0; y < bmp1.Height; y++)
+            {
+                for(int x = 0; x < bmp1.Width; x++)
+                {
+                    int zmienna = bmp2.GetPixel(x, y).R;
+                    if (bmp1.GetPixel(x, y).R < zmienna - 10 || bmp1.GetPixel(x, y).R > zmienna + 10)
+                    {
+                            return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        void AnyMovement()
+        {
+            while (true)
+            {
+                while (!anyMovement)
+                {
+                    System.Threading.Thread.Sleep(100);
+                }
+                g = Graphics.FromImage(terenDoSzukania);
+                g.CopyFromScreen(punkt1.X, punkt1.Y, 0, 0, terenDoSzukania.Size);
+                g.Dispose();
+
+                while (anyMovement)
+                {
+                    g = Graphics.FromImage(terenDoPorownania);
+                    g.CopyFromScreen(punkt1.X, punkt1.Y, 0, 0, terenDoPorownania.Size);
+                    g.Dispose();
+                    if (!CompareBitmaps(terenDoSzukania, terenDoPorownania))
+                    {
+                        System.Threading.Thread.Sleep((int)delay.Value);
+                        if (klawisz.Text == "LeftClick")
+                        {
+                            mouse_event(MOUSEEVENTF_LEFTDOWN, (uint)Cursor.Position.X, (uint)Cursor.Position.Y, 0, 0);
+                            System.Threading.Thread.Sleep(10);
+                            mouse_event(MOUSEEVENTF_LEFTUP, (uint)Cursor.Position.X, (uint)Cursor.Position.Y, 0, 0);
+                            anyMovement = !anyMovement;
+                        }
+                        else
+                        {
+                            SendKeys.SendWait(klawisz.Text);
+                            break;
+                        }
+                    }
+                }
+                System.Threading.Thread.Sleep((int)waitTime.Value);
+            }
+        }
+        void FindAndDo()
+        {
+            while (true)
+            {
+                if (castOnStart.Checked)
+                {
+                    System.Threading.Thread.Sleep((int)waitTime.Value);
+                    if (klawisz.Text == "LeftClick")
+                    {
+                        mouse_event(MOUSEEVENTF_LEFTDOWN, (uint)Cursor.Position.X, (uint)Cursor.Position.Y, 0, 0);
+                        System.Threading.Thread.Sleep(10);
+                        mouse_event(MOUSEEVENTF_LEFTUP, (uint)Cursor.Position.X, (uint)Cursor.Position.Y, 0, 0);
+                    }
+                    else
+                    {
+                        SendKeys.SendWait(klawisz.Text);
+                    }
+                    System.Threading.Thread.Sleep((int)delay.Value);
+                }
+                while (true)
+                {
+                    punkt3 = ColorFinder();
+                    g.Dispose();
+                    if (punkt3.X == 0 && punkt3.Y == 0)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        if (pointAndClick.Checked)
+                        {
+                            Cursor.Position = punkt3;
+                            mouse_event(MOUSEEVENTF_LEFTDOWN, (uint)punkt3.X, (uint)punkt3.Y, 0, 0);
+                            System.Threading.Thread.Sleep(10);
+                            mouse_event(MOUSEEVENTF_LEFTUP, (uint)punkt3.X, (uint)punkt3.Y, 0, 0);
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+
+        void FindAndWait()
+        {
+            bitmap = new Bitmap(10, 10);
+            while (true)
+            {
+                if (castOnStart.Checked)
+                {
+                    System.Threading.Thread.Sleep((int)waitTime.Value);
+                    if (klawisz.Text == "LeftClick")
+                    {
+                        mouse_event(MOUSEEVENTF_LEFTDOWN, (uint)Cursor.Position.X, (uint)Cursor.Position.Y, 0, 0);
+                        System.Threading.Thread.Sleep(10);
+                        mouse_event(MOUSEEVENTF_LEFTUP, (uint)Cursor.Position.X, (uint)Cursor.Position.Y, 0, 0);
+                    }
+                    else
+                    {
+                        SendKeys.SendWait(klawisz.Text);
+                    }
+                    System.Threading.Thread.Sleep((int)delay.Value);
+                }
+                punkt3 = ColorFinder();
+                g.Dispose();
+                if (punkt3.X == 0 && punkt3.Y == 0)
+                {
+                    Console.WriteLine("Color not found");
+                    continue;
+                }
+                while (ColorFinder(punkt3))
+                {
+                    System.Threading.Thread.Sleep(5);
+                }
+                g.Dispose();
+                punkt3.X -= 10;
+                if (pointAndClick.Checked)
+                {
+                    Cursor.Position = punkt3;
+                    mouse_event(MOUSEEVENTF_LEFTDOWN, (uint)punkt3.X, (uint)punkt3.Y, 0, 0);
+                    System.Threading.Thread.Sleep(10);
+                    mouse_event(MOUSEEVENTF_LEFTUP, (uint)punkt3.X, (uint)punkt3.Y, 0, 0);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ghk = new KeyHandler(Keys.K, this);
+            ghk.Unregiser();
+            ghk = new KeyHandler(Keys.J, this);
+            ghk.Unregiser();
+            ghk = new KeyHandler(Keys.H, this);
+            ghk.Unregiser();
+
+            terenDoSzukania = new Bitmap(punkt2.X - punkt1.X, punkt2.Y - punkt1.Y);
+
+            if (radioButton1.Checked)
+            {
+                Thread thread_1 = new Thread(new ThreadStart(FindAndDo));
+                thread_1.IsBackground = true;
+                thread_1.Start();
+            }
+            else
+            {
+                if (radioButton2.Checked)
+                {
+                    Thread thread_1 = new Thread(new ThreadStart(FindAndWait));
+                    thread_1.IsBackground = true;
+                    thread_1.Start();
+                }
+                else
+                {
+                    ghk = new KeyHandler((Keys)converter.ConvertFromString(textBox1.Text), this);
+                    ghk.Register();
+                    Thread thread_1 = new Thread(new ThreadStart(AnyMovement));
+                    thread_1.IsBackground = true;
+                    thread_1.Start();
+                }
+            }
+            
+            button1.Enabled = false;
+            castOnStart.Checked = true;
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (klawisz.Focused)
+            {
+                klawisz.Text = "" + e.KeyCode;
+            }
+            if (textBox1.Focused)
+            {
+                textBox1.Text = "" + e.KeyCode;
+            }
+        }
+
+        private void klawisz_DoubleClick(object sender, EventArgs e)
+        {
+            klawisz.Text = "LeftClick";
+        }
+    }
+}
